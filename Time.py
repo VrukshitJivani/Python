5ddd9ef4f3510f32fc26252d623c46b806d9850b
Vrukshit Jivani <vjivani2233@gmail.com>
2026-01-10 19:10:54 +0530

    Add files via upload

---
+++ Time.py
@@ -0,0 +1,6 @@
+time=int(input("Enter Minutes:"))
+print("Minutes is ",time)
+
+hour=time//60
+minut=time%60
+print(hour,"Hours",' ',minut,'Minut')
---
+++ Weigth.py
@@ -0,0 +1,7 @@
+grams=int(input("Enter Grams:"))
+print("Grams is ",grams)
+
+kg=grams//1000
+grams=grams-(kg*1000)
+print(kg,"Kg",' ',grams,'Grams')
+
