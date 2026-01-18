35dc548a3282e1179fe73b58d723e3fe91936784
Vrukshit Jivani <vjivani2233@gmail.com>
2026-01-18 19:01:58 +0530

    Add files via upload

---
+++ if_leap_year.py
@@ -0,0 +1,6 @@
+year=int(input("Enter year:"))
+print(year)
+if year%4==0:
+    print("This is a leap year")
+else:
+    print("This is not leap year")
---
+++ if_vote.py
@@ -0,0 +1,6 @@
+age=int(input("Enter age for check eligibility for vote:"))
+print(age)
+if age>18:
+    print("You are eligibile for vote")
+else:
+    print("You are not eligible for vote")
