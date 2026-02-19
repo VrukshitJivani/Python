1ef14b2f266d84412e50c8eb08ebefad422107a3
Vrukshit Jivani <vjivani2233@gmail.com>
2026-02-19 21:02:50 +0530

    Add files via upload

---
+++ modul/function_modul_3.py
@@ -0,0 +1,13 @@
+import math
+
+no=31.12
+
+print(f"ceil value of no {no} is ",math.ceil(no))
+print(f"floor value of no {no} is ",math.floor(no))
+print(f"truncate value of no {-31.23} is ",math.trunc(-31.23))
+print(f"absolute value of no {-12} is ",math.fabs(-12))
+print(f"factorial value of no {5} is ",math.factorial(5))
+print(f"copy sign of no {-1} is ",math.copysign(no,-1))
+print(f"modf value output  is ",math.modf(no))
+print(f"reminder of 45 divide by 6 is ",math.fmod(45,6))
+
